ef28946491d4826294e6f305841149a5823fd2ea
Yash Yuvraj <yashyuvrajsanju@gmail.com>
2023-04-24 14:55:49 +0530

    Add files via upload

---
+++ yashyuvrajsanju-ME_QEATS_V2-master/qeatsbackend/src/main/java/com/crio/qeats/services/RestaurantServiceImpl.java
@@ -0,0 +1,54 @@
+
+/*
+ *
+ *  * Copyright (c) Crio.Do 2019. All rights reserved
+ *
+ */
+
+package com.crio.qeats.services;
+
+import com.crio.qeats.dto.Restaurant;
+import com.crio.qeats.exchanges.GetRestaurantsRequest;
+import com.crio.qeats.exchanges.GetRestaurantsResponse;
+import com.crio.qeats.repositoryservices.RestaurantRepositoryService;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Log4j2
+public class RestaurantServiceImpl implements RestaurantService {
+
+  private final Double peakHoursServingRadiusInKms = 3.0;
+  private final Double normalHoursServingRadiusInKms = 5.0;
+  @Autowired
+  private RestaurantRepositoryService restaurantRepositoryService;
+
+
+  // TODO: CRIO_TASK_MODULE_RESTAURANTSAPI - Implement findAllRestaurantsCloseby.
+  // Check RestaurantService.java file for the interface contract.
+  @Override
+  public GetRestaurantsResponse findAllRestaurantsCloseBy(
+      GetRestaurantsRequest getRestaurantsRequest, LocalTime currentTime) {
+          int hour=currentTime.getHour();
+          int min=currentTime.getMinute();
+          double radius=5.0;
+          if((hour>=8&&hour<=9)||(hour==10&&min==0)||(hour==13)||(hour==14&&min==0)||(hour>=19&&hour<=20)||(hour==21&&min==0))
+          radius=3.0;
+            return new GetRestaurantsResponse(restaurantRepositoryService.findAllRestaurantsCloseBy(getRestaurantsRequest.getLatitude(), getRestaurantsRequest.getLongitude(), currentTime, radius));
+          
+    // return null;
+  }
+
+
+}
+
